71870b28f5912ec777d3937cb6e41de9215b48b1
DmitryKotusev <dssa-1966@yandex.ru>
2019-11-29 23:44:55 +0300

    Added Odin package.

---
+++ Assets/Scripts/SoldierBasic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class SoldierBasic : MonoBehaviour
+{
+    [BoxGroup("Bones")]
+    [SerializeField]
+    GameObject rootBone;
+
+    [BoxGroup("Bones")]
+    [SerializeField]
+    GameObject headBone;
+
+    [SerializeField]
+    GameObject helmet;
+
+    [SerializeField]
+    Team team;
+
+    public Team Team
+    {
+        get
+        {
+            return team;
+        }
+    }
+}
