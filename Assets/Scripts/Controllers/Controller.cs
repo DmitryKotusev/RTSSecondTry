f0889575424eb662d2f95c2be41650317bc87380
DmitryKotusev <dssa-1966@yandex.ru>
2019-12-04 02:55:55 +0300

    Added projection shader.

---
+++ Assets/Scripts/Controllers/Controller.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+/// <summary>
+/// Basic class for controller entity.
+/// It can either be human player or
+/// AI.
+/// </summary>
+abstract public class Controller : MonoBehaviour
+{
+    static HashSet<short> controllerIdsPool = new HashSet<short>();
+
+    [SerializeField]
+    Team team;
+
+    [SerializeField]
+    [ReadOnly]
+    short controllerId;
+
+    public Controller()
+    {
+        controllerId = GetFreeId();
+        RegisterNewId(controllerId);
+    }
+
+    // Static methods
+    #region
+    /// <summary>
+    /// Returns free id.
+    /// Returns 0 if no free ids are available.
+    /// If it returns 0 something might actually
+    /// went wrong.
+    /// </summary>
+    /// <returns></returns>
+    static short GetFreeId()
+    {
+        for (short i = 1; i <= short.MaxValue; i++)
+        {
+            if (!controllerIdsPool.Contains(i))
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    static bool IsIdFree(short id)
+    {
+        return !controllerIdsPool.Contains(id);
+    }
+
+    static void RegisterNewId(short id)
+    {
+        if (!IsIdFree(id))
+        {
+            throw new System.Exception($"Id {id} already belongs to other controller");
+        }
+    }
+
+    static bool UnregisterId(short id)
+    {
+        return controllerIdsPool.Remove(id);
+    }
+    #endregion
+
+    // Getters and setters
+    #region
+    public void SetTeam(Team team)
+    {
+        this.team = team;
+    }
+
+    public Team GetTeam()
+    {
+        return team;
+    }
+
+    public void SetControllerId(short controllerId)
+    {
+        if (this.controllerId == controllerId)
+        {
+            return;
+        }
+
+        RegisterNewId(controllerId);
+        UnregisterId(this.controllerId);
+
+        this.controllerId = controllerId;
+    }
+
+    public short GetControllerId()
+    {
+        return controllerId;
+    }
+    #endregion
+
+    ~Controller()
+    {
+        UnregisterId(controllerId);
+        Debug.Log($"Controller with id {controllerId} destroyed");
+    }
+}
---
+++ Assets/Shaders/ProjectionShaders/ProjectShader3.shader
@@ -0,0 +1,66 @@
+﻿Shader "Projector/Custom" {
+	Properties{
+		_Color("Tint Color", Color) = (1,1,1,1)
+		_ShadowTex("Cookie", 2D) = "white" {}
+		_Attenuation("Falloff", Range(0.0, 1.0)) = 1.0
+	}
+
+		Subshader{
+			Tags {
+				"RenderType" = "Transparent"
+				"Queue" = "Transparent+100"
+			}
+			Pass {
+				ZWrite Off
+				Offset -1, -1
+
+				Fog{ Mode Off }
+
+				ColorMask RGB
+				Blend OneMinusSrcAlpha SrcAlpha
+
+				CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				#pragma fragmentoption ARB_fog_exp2
+				#pragma fragmentoption ARB_precision_hint_fastest
+				#include "UnityCG.cginc"
+
+				struct v2f
+				{
+					float4 pos : SV_POSITION;
+					float4 uv : TEXCOORD0;
+				};
+
+				sampler2D _ShadowTex;
+				float4x4 unity_Projector;
+				float4 _Color;
+				float _Attenuation;
+
+				v2f vert(appdata_tan v)
+				{
+					v2f o;
+					o.pos = UnityObjectToClipPos(v.vertex);
+					o.uv = mul(unity_Projector, v.vertex);
+					return o;
+				}
+
+				half4 frag(v2f i) : COLOR
+				{
+					// float depth = i.uv.z; // [-1 (near), 1 (far)]
+
+					half4 tex = tex2Dproj(_ShadowTex, i.uv);
+					// tex.a = (1 - tex.a * _Attenuation);
+					tex.a = (1 - tex.a * _Attenuation);
+					if (i.uv.w < 0)
+					{
+						tex = float4(0, 0, 0, 1);
+					}
+
+					tex = float4(_Color.r, _Color.g, _Color.b, tex.a);
+					return tex;
+				}
+				ENDCG
+			}
+	}
+}
