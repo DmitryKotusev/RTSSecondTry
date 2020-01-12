763144a061fcc005ce5eb2f5cafc47d2281ca54b
DmitryKotusev <dssa-1966@yandex.ru>
2020-01-12 03:06:00 +0300

    Making UI for giving commands to units.

---
+++ Assets/Scripts/GUIUtils/MouseCursorHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class MouseCursorHandler : MonoBehaviour
+{
+    [BoxGroup("Cursor textures")]
+    [SerializeField]
+    [Tooltip("Attack cursor")]
+    Texture2D attackCursor;
+}
