1797dd51db54b6dda6e0c450ec784b1ddaba8076
DmitryKotusev <dssa-1966@yandex.ru>
2020-03-22 22:27:01 +0300

    Started making spawn points.

---
+++ Assets/Scripts/ScriptableObjects/SpawnGroup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CustomScriptables/SpawnGroup")]
+public class SpawnGroup : ScriptableObject
+{
+    [SerializeField]
+    private List<KeyValuePair<GameObject, int>> spawnPairs;
+
+    public List<KeyValuePair<GameObject, int>> SpawnPairs => spawnPairs;
+}
---
+++ Assets/Scripts/SpawnPoint/SpawnPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField]
+    private Controller controller;
+
+    public Controller Controller
+    {
+        get => controller;
+        set => controller = value;
+    }
+
+    public void SpawnGroup(SpawnGroup spawnGroup)
+    {
+
+    }
+}
